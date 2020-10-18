29f29c0fec91d78d4bc453ccd7adb19dceb19648
Yuri Faria <yuriluisfaria@gmail.com>
2020-10-18 20:45:51 -0300

    fixing bug for not putting arguments in tuple

--- delphin-rdf/delphin/rdf/eds_parser.py
+++ delphin-rdf/delphin/rdf/eds_parser.py
@@ -14,58 +14,59 @@ ERG = Namespace("http://www.delph-in.net/schema/erg#")
 
 def __nodes_to_rdf__(eds, graph, NODES):
     """
-    Creates the nodes of the EDS and their properties.
+    Creates nodes of variables and nodes specifying their properties.
 
     eds - a delphin eds instance to be parsed into RDF format.
     
     graph - and rdflib graph that is used to store the EDS as RDF
     representation.
 
-    NODES - the IRI namespace dedicated to nodes.
+    NODES - the URI namespace dedicated to nodes.
     """
-    nodeIRI = NODES[node.id]
-    nodePredIRI = NODES[node.id + "#predicate"]
     for node in eds.nodes:
+        nodeIRI = NODES[node.id]
+        nodePredIRI = NODES[node.id + "#predicate"]
+        
         #Instantiate the Node
-        graph.add(nodeIRI, RDF.type, EDS.Node)
+        graph.add((nodeIRI, RDF.type, EDS.Node))
         
         #typing the predicate
         if delphin.predicate.is_surface(node.predicate):
-            graph.add(nodePredIRI, RDF.type, EDS.SurfacePredicate)
+            graph.add((nodePredIRI, RDF.type, EDS.SurfacePredicate))
         elif delphin.predicate.is_abstract(node.predicate):
-            graph.add(nodePredIRI, RDF.type, EDS.AbstractPredicate)
+            graph.add((nodePredIRI, RDF.type, EDS.AbstractPredicate))
         else: #not surface neither abstract
             print("{} is an invalid predicate".format(node.predicate))
-            graph.add(nodePredIRI, RDF.type, EDS.NodePredicate)
+            graph.add((nodePredIRI, RDF.type, EDS.NodePredicate))
             
         #Declaring the node predicate
-        graph.add(nodeIRI, EDS.hasNodePredicate, nodePredIRI)
+        graph.add((nodeIRI, EDS.hasNodePredicate, nodePredIRI))
         
         # surface and its parts:
         splittedPredicate = delphin.predicate.split(delphin.predicate.normalize(node.predicate))
-        graph.add(nodePredIRI, EDS.hasLemma, Literal(splittedPredicate[0]))
+        graph.add((nodePredIRI, EDS.hasLemma, Literal(splittedPredicate[0])))
         if splittedPredicate[1]:
-            graph.add(nodePredIRI, EDS.hasPos, EDS[splittedPredicate[1]])
+            graph.add((nodePredIRI, EDS.hasPos, EDS[splittedPredicate[1]]))
         if splittedPredicate[2]:
-            graph.add(nodePredIRI, EDS.hasSense, Literal(splittedPredicate[2]))
+            graph.add((nodePredIRI, EDS.hasSense, Literal(splittedPredicate[2])))
         
         #lnk:
         if node.cfrom:
-            graph.add(nodeIRI, EDS.cfrom, Literal(node.cfrom))
+            graph.add((nodeIRI, EDS.cfrom, Literal(node.cfrom)))
         if node.cto:
-            graph.add(nodeIRI, EDS.cto, Literal(node.cto))
+            graph.add((nodeIRI, EDS.cto, Literal(node.cto)))
         
         # type of node:
         if node.type:
-            graph.add(nodeIRI, EDS.hasNodeType, EDS[])
+            graph.add((nodeIRI, EDS.hasNodeType, Literal(EDS[node.type])))
         
         # properties
         for prop in node.properties.items():
-            graph.add(nodeIRI, EDS[prop[0].lower()], Literal(prop[1].lower()))
+            graph.add((nodeIRI, EDS[prop[0].lower()], Literal(prop[1].lower())))
             
         # carg
         if node.carg:
-            graph.add(nodeIRI, EDS.carg, Literal(node.carg))
+            graph.add((nodeIRI, EDS.carg, Literal(node.carg)))
 
 
 def __edges_to_rdf__(eds, graph, NODES):
@@ -80,7 +81,7 @@ def __edges_to_rdf__(eds, graph, NODES):
     NODES - the IRI namespace dedicated to nodes.
     """
     for edge in eds.edges:
-        graph.add(NODES[edge[0]], EDS[edge[1].lower()], NODES[edge[2]])
+        graph.add((NODES[edge[0]], EDS[edge[1].lower()], NODES[edge[2]]))
         
 
         
